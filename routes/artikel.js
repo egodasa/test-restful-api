87c3c7a06a813ac86d09e328858192f1ff5c804d
egodasa <egodasa@gmail.com>
2018-02-26 20:52:42 +0700

    Fix query untuk menghitung total record

--- routes/artikel.js
+++ routes/artikel.js
@@ -10,7 +10,7 @@ const { checkSchema, validationResult } = require('express-validator/check');
 const SQLBuilder = require('json-sql-builder');
 var sqlbuilder   = new SQLBuilder('mysql');
 let _ = require('lodash')
-
+let countAlias = 'COUNT(`'+pk+'`)'
 /* GET users listing. */
 router.get('/:id?', checkSchema(validator.paginasi) ,asing(async(req, res, next)=> {
 	let id = req.params.id;
@@ -18,9 +18,17 @@ router.get('/:id?', checkSchema(validator.paginasi) ,asing(async(req, res, next)
 	let offset = req.query.offset || 0;
 	let error = validationResult(req)
 	let json_query = { //base query
-		$select : {
-			$columns : field,
-			$from : tb
+			select : {
+				$select : {
+					$columns : field,
+					$from : tb
+					}
+			},
+			count : {
+				$select : {
+					$columns : [{$count : pk}],
+					$from : tb
+					}
 			}
 		}
 	let whereField = {} //kolom where
@@ -33,7 +41,8 @@ router.get('/:id?', checkSchema(validator.paginasi) ,asing(async(req, res, next)
 		_.each(whereFieldTmp, (v)=>{ //iterasi kolom tersebut (array) (field1)
 			whereField[v] = {$like : '%'+req.query[v]+'%'} //menjadikan query url tadi menjadi object {field1 : value1}, ketentuan where like %value%
 			})
-		json_query.$select.$where = {$or : [whereField]} //tambahkan hasil object where ke base query
+		json_query.select.$select.$where = {$or : [whereField]} //tambahkan hasil object where ke base query
+		json_query.count.$select.$where = {$or : [whereField]} //tambahkan hasil object where ke count query (total rows)
 		}
 	if(orderFieldTmp.length != 0){  //jika kolom orderby ada
 		if(orderFieldTmp instanceof Array){ //cek apakah kolom orderby satu atau lebih (array)
@@ -45,24 +54,27 @@ router.get('/:id?', checkSchema(validator.paginasi) ,asing(async(req, res, next)
 			let tmp = _.split(orderFieldTmp, ':', 2) //jika tidak array atau satu, langsung pecah nilainya orderBy=field:desc
 			orderField[tmp[0]] = tmp[1] //mengubah query url orderby tadi jadi object {field:desc}
 		}
-		json_query.$select.$sort = orderField //masukkan hasil object order ke base query
+		json_query.select.$select.$sort = orderField //masukkan hasil object order ke base query
+		json_query.count.$select.$sort = orderField //masukkan hasil object order ke count query
 	}
-	json_query.$select.$limit = limit
-	json_query.$select.$offset = offset
-	let finalQuery = sqlbuilder.build(json_query) //finalQuery.sql isi query, finalQuery.values untuk value yang aka di pass ke knex.raw(sql, value)
+	json_query.select.$select.$limit = Number(limit) //count query tidak pakai limit dan offset
+	json_query.select.$select.$offset = Number(offset)
+	let finalQuerySelect = sqlbuilder.build(json_query.select) //finalQuery.sql isi query, finalQuery.values untuk value yang aka di pass ke knex.raw(sql, value)
+	let finalQueryCount = sqlbuilder.build(json_query.count) //finalQuery untuk count query
+	
 	if(!error.isEmpty()){ //validasi nilai limit dan offset untuk paginasi
 		res.status(422).json({error : error.mapped()})
 		}
 	else{
 		let status;
 		let query = {
-			count : id ? db(tb).count(pk + ' as jumlah').where(pk, id) : db(tb).count(pk + ' as jumlah'),
-			select : id ? db(tb).select().where(pk, id) : db.raw(finalQuery.sql, finalQuery.values) // hasil akhir query
+			count : id ? db(tb).count(pk).where(pk, id) : db.raw(finalQueryCount.sql, finalQueryCount.values),
+			select : id ? db(tb).select().where(pk, id) : db.raw(finalQuerySelect.sql, finalQuerySelect.values) // hasil akhir query
 			}
 		let row = await query.count; //Hitung total record
 		let record = await query.select; //Record yang diambil
-		row[0].jumlah == 0 ? status = 204 : status = 200; //http code
-		res.status(status).json({data : record,record_total:row[0].jumlah});
+		row[0] == 0 ? status = 204 : status = 200; //http code 204 jika data kosong 
+		res.status(status).json({data : record[0],total_rows:row[0][0][countAlias]});
 	}
 }));
 router.post('/', checkSchema(validasiData), asing(async(req, res, next)=> {
